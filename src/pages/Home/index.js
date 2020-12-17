75ad591c109245835d8aa95d8fe120ea741d99ae
vladimiremi <vladimiremi2014@gmail.com>
2020-12-17 08:13:17 -0300

    Chore: add Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../../assents/logo.png';
+import './styles.css';
+
+export default function Home(){
+    return(
+        <div>
+            <header className="home-header">
+                <img src={logo} alt="logo"/>
+                <section className="container">
+                    <Link className="home-button" to="/register">
+                        Anunciar
+                    </Link>
+                    <Link className="home-button" to="/logon">
+                        Criar conta
+                    </Link>
+                </section>
+            </header>
+
+        </div>
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
+import Home from './pages/Home';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Logon} />
+                <Route path="/" exact component={Home} />
+                <Route path="/logon" exact component={Logon} />
                 <Route path="/register" component={Register} />
             </Switch>
         </BrowserRouter>
