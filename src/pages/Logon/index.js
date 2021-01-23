c8d49f3956f293d1fa41baddf6d5600a9bb15eaa
vladimiremi <vladimiremi2014@gmail.com>
2021-01-22 22:47:37 -0300

    Feat: connection Logon

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import './styles.css';
 import logo from '../../assents/logo.png';
+import api from '../../services/api';
 
 export default function Logon(){
+
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogon(f){
+        f.preventDefault();
+
+        
+        const response = await api.post('sessions', { id } );
+
+        localStorage.setItem('agricultorId', id);
+        localStorage.setItem('agricultorNickname', response.data.nickname)
+
+        console.log(response.data.nickname);
+        history.push('/user'); //direciona o usuário para essa rota
+
+    }
 
     return(
         <div className="done">
             <div className="logo">
                 <img src={logo} alt="Done"/>
             </div>            
-            <form className="inputs">
-                    <input className="input" type="input" placeholder="ID"/>
-                    <Link className="button" to="/user">Entrar</Link>
+            <form className="inputs" onSubmit={handleLogon}>
+                    <input 
+                        className="input" 
+                        type="input" 
+                        placeholder="ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+                    <button className="button" to="/user">Entrar</button>
                     <p> <strong>Não tenho conta:</strong></p>
                     <Link className="link" to="/register">Criar Conta</Link>
             </form>
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -4,11 +4,13 @@ import logo from '../../assents/logo.png';
 import './styles.css';
 import { MdDeleteForever } from 'react-icons/md';
 export default function Home(){
+    const nickname = localStorage.getItem('agricultorNickname');
+
     return(
         <div> 
             <header className="home-header">
                 <img src={logo} alt="logo"/>
-                <p>Bem vindo, Seu João!</p>
+                <p>Bem vindo, Seu {nickname}!</p>
                 <section className="container">
                     <Link className="home-button" to="/newadvertisement">
                         Anunciar
